eb36ce18c8be8fdcb04d3ff6251a76f827de978f
pranavkharche79 <pranavkharche79@gmail.com>
2023-12-28 14:48:57 +0530

    done

--- Pranav_DotNet/Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
+++ Pranav_DotNet/Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe8972ecc92e414c8b2b1f9fcdbccd42845c112")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
